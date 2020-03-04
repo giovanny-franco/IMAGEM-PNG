0315d5305d51b6b3114a6ff84abbcf421c5d8f5e
giovanny-franco <60987463+giovanny-franco@users.noreply.github.com>
2020-03-04 18:15:44 -0300

    Add files via upload

---
+++ tuxtux/src/tuxtux/Tuxtux.java
@@ -0,0 +1,12 @@
+package tuxtux;
+import javax.swing.JFrame;
+public class Tuxtux {
+
+    public static void main(String[] args) {
+       LabelFrame labelframe = new LabelFrame();
+       labelframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       labelframe.setSize(1280, 720);
+       labelframe.setVisible(true);
+    }
+    
+}
